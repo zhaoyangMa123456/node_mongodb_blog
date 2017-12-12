82eeeafd2c707a4d31687467a85bb2a3466cd82f
zhaoyangMa123456 <414739610@qq.com>
2017-12-12 14:18:10 +0800

    整理jade引擎规则

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    res.render('index', { title: 'Express' });
+    var data = {
+        text: {
+            book:{
+                name: '藏地密码',
+                price: '98.99'
+            }
+        },
+        variable: 'mazhaoyang.cn',
+        inputVals: {
+            type: "text",
+            name: "马朝阳"
+        },
+        ifElse: {
+            name: 'mzy'
+        },
+        forEachVals: ['a', 'b', 'c'],
+        caseVals: 'mzy'
+    }
+    res.render('index', data);
 });
 
 module.exports = router;
