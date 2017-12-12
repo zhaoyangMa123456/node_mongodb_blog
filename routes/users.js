0e920395a0570ac118397705efbd2dfc80cef8a3
zhaoyangMa123456 <414739610@qq.com>
2017-12-12 18:02:50 +0800

    整理jade引擎规则

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/add', function(req, res, next) {
+  res.send('add');
+});
 
 module.exports = router;
